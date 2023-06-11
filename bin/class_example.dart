a6b1badc94a8ce86b619970e4aaaf25a7e0f3d57
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-11 19:36:58 +0530

    get

--- bin/class_example.dart
+++ bin/class_example.dart
@@ -2,8 +2,8 @@ import 'person.dart';
 
 void main() {
   final person = Person('sudeep', 30);
-  print(person._name); //private variable is not accessible outside the class
-  print(person._age);
+  //print(person._name); //private variable is not accessible outside the class
+  print(person.helloage);
 
   person.sayHello();
 }
--- bin/person.dart
+++ bin/person.dart
@@ -3,6 +3,14 @@ class Person {
   final int _age;
 
   Person(this._name, this._age);
+  int get helloage {
+    //getiing the value of private variable
+    return _age;
+  }
+
+  int getAge() {
+    return _age;
+  }
 
   // Person.ageBelow50(this.name, this.age) {
   //   print(name);
